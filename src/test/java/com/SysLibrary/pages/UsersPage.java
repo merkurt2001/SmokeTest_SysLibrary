5df3c42d2953b935f5a0817f19ed687d070de454
merkurt2001 <merkurt2001@gmail.com>
2021-02-11 09:04:22 +0300

    UsersPage locaters added

--- src/test/java/com/SysLibrary/pages/UsersPage.java
+++ src/test/java/com/SysLibrary/pages/UsersPage.java
@@ -1,5 +1,37 @@
 package com.SysLibrary.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class UsersPage extends BasePage{
 
+    @FindBy(css =".btn.btn-lg.btn-outline.btn-primary.btn-sm")
+    public WebElement addBooksButton;
+
+    @FindBy(css ="[name='full_name']")
+    public WebElement userName;
+
+    @FindBy(css ="[name='password']")
+    public WebElement password;
+
+    @FindBy(css ="[name='email']")
+    public WebElement email;
+
+    @FindBy(id = "user_group_id")
+    public WebElement userGroupId;
+
+    @FindBy(id = "status")
+    public WebElement status;
+
+    @FindBy(id = "start_date")
+    public WebElement startDate;
+
+    @FindBy(id = "end_date")
+    public WebElement endDate;
+
+    @FindBy(id = "address")
+    public WebElement address;
+
+    @FindBy(css = "[type='submit']")
+    public WebElement submit;
 }
