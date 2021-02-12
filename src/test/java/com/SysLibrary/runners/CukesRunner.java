4682b75b6495b625c5da6ab25ad13f5e93807792
erenduygu <erenduygu33@gmail.com>
2021-02-12 22:36:39 +0300

    @smoke1234 added

--- src/test/java/com/SysLibrary/runners/CukesRunner.java
+++ src/test/java/com/SysLibrary/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/SysLibrary/step_definitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@wip @smoke1234"
 )
 public class CukesRunner {
 }
